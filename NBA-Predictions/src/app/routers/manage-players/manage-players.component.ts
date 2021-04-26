499f059000cf9244c58c293883f2cc83dbddce8d
102560056 <102560056@student.swin.edu.au>
2021-04-26 12:52:01 +1000

    Fixed team summary, and table headers. Added jquery settup.

--- NBA-Predictions/src/app/routers/manage-players/manage-players.component.ts
+++ NBA-Predictions/src/app/routers/manage-players/manage-players.component.ts
@@ -10,7 +10,10 @@ import { Player } from '../../modules/player';
 import { PlayerEnvelope } from 'src/app/modules/playerEnvelope';
 import { browserRefresh } from '../../app.component';
 import { faSort } from '@fortawesome/free-solid-svg-icons';
+import { faSortDown } from '@fortawesome/free-solid-svg-icons';
+import { faSortUp } from '@fortawesome/free-solid-svg-icons';
 import { PlayerSelections } from 'src/app/modules/playerSelections';
+import * as $ from "jquery";
 
 @Component({
   selector: 'app-manage-players',
@@ -18,6 +21,10 @@ import { PlayerSelections } from 'src/app/modules/playerSelections';
   styleUrls: ['./manage-players.component.css']
 })
 export class ManagePlayersComponent implements OnInit {
+
+  faSortIcon = faSort;
+  faSortIconUp = faSortUp;
+  faSortIconDown = faSortDown;
 
   teamName: string;
   players = [];
@@ -32,62 +39,64 @@ export class ManagePlayersComponent implements OnInit {
   selectedPlayers: Player[] = [];
   selectedPlayersKeys: number[] = [];
   checked = false;
-  refreshed : boolean;
-  sorticon = faSort;
+  refreshed: boolean;
   playerSearched: boolean = false;
-  searchString? : string = '';
-  playerSelection : PlayerSelections;
+  searchString?: string = '';
+  playerSelection: PlayerSelections;
 
   constructor(private httpService: HttpService, private navService: NavService, private currentTeamService: CurrentTeamService) {
-    // this.teamName = this.currentTeamService.teamName;
-
+    this.teamName = this.currentTeamService.teamName;
     //detects browser refresh
-    this.refreshed = browserRefresh;
+
+
+    // Why are we refreshing again??
+    // this.refreshed = browserRefresh;
   }
 
   ngOnInit(): void {
     this.players = this.httpService.ViewPlayers(this.pageNum, this.pageSize, this.sortString, this.sortOrder);
     this.headers = this.httpService.GetPlayerHeaders();
     this.selectedPlayers = this.currentTeamService.players;
+    console.log(this.selectedPlayers)
+    console.log(this.players);
+
+    console.log(this.players.includes(this.selectedPlayers));
 
     //used an observable to get the pages and localstorage to keep the pages on refresh
     of(null).pipe(delay(650)).subscribe(() => {
-      if(this.refreshed == false){
+      if (this.refreshed == false) {
         this.pages = this.httpService.pages;
         localStorage.setItem('pages', JSON.stringify(this.pages));
       }
-      else if(localStorage.getItem('pages') != null && Number(JSON.parse(localStorage.getItem('pages'))) > 1)
-      {
+      else if (localStorage.getItem('pages') != null && Number(JSON.parse(localStorage.getItem('pages'))) > 1) {
         this.pages = Number(JSON.parse(localStorage.getItem('pages')));
       }
-     
+
     });
 
+    // This isnt needed. Thats what the current team service is for
     //get Team name
-    of(null).pipe(delay(300)).subscribe(() => {
-      this.teamName = JSON.parse(localStorage.getItem('team'));
-    });
-
+    // of(null).pipe(delay(300)).subscribe(() => {
+    //   this.teamName = JSON.parse(localStorage.getItem('team'));
+    // });
   }
 
-  ngOnDestroy(){
+  ngOnDestroy() {
     //remove the item when component is destroyed
     localStorage.removeItem('pages');
-    localStorage.removeItem('team');
+    // localStorage.removeItem('team');
   }
 
 
-  Searching(searchString: string) {
-    this.searchString = searchString;
+  Searching() {
     this.pageNum = 1;
-
-    if(searchString == '' || searchString == null){
+    if (this.searchString == '' || this.searchString == null) {
       alert("No search string was provided");
       return null;
     }
     this.playerSearched = true;
     this.players = this.httpService.PlayerSearch(this.pageNum, this.pageSize, this.searchString, this.sortString, this.sortOrder);
-    
+
     of(null).pipe(delay(500)).subscribe(() => {
       this.pages = Number(JSON.parse(localStorage.getItem('playerSearchPages')));
     });
@@ -97,10 +106,10 @@ export class ManagePlayersComponent implements OnInit {
   IncreasePage() {
     if (this.pageNum < this.pages) {
       this.pageNum += 1;
-      if(this.playerSearched == false){
+      if (this.playerSearched == false) {
         this.players = this.httpService.ViewPlayers(this.pageNum, this.pageSize, this.sortString, this.sortOrder);
       }
-      else{
+      else {
         this.players = this.httpService.PlayerSearch(this.pageNum, this.pageSize, this.searchString, this.sortString, this.sortOrder);
       }
     }
@@ -109,10 +118,10 @@ export class ManagePlayersComponent implements OnInit {
   DecreasePage() {
     if (this.pageNum > 1) {
       this.pageNum -= 1;
-      if(this.playerSearched == false){
+      if (this.playerSearched == false) {
         this.players = this.httpService.ViewPlayers(this.pageNum, this.pageSize, this.sortString, this.sortOrder);
       }
-      else{
+      else {
         this.players = this.httpService.PlayerSearch(this.pageNum, this.pageSize, this.searchString, this.sortString, this.sortOrder);
       }
     }
@@ -138,14 +147,14 @@ export class ManagePlayersComponent implements OnInit {
       this.activeUpSort = sortElement;
       this.sortOrder = 'ASC';
     }
-    
-    if(this.searchString == ''){
+
+    if (this.searchString == '') {
       this.players = this.httpService.ViewPlayers(this.pageNum, this.pageSize, this.sortString, this.sortOrder);
       return;
     }
-    else if(this.searchString != '' && this.sortOrder == 'DESC') {
+    else if (this.searchString != '' && this.sortOrder == 'DESC') {
       this.players = this.httpService.PlayerSearch(this.pageNum, this.pageSize, this.searchString, this.sortString, this.sortOrder);
-    }   
+    }
   }
 
   ManageSelectedPlayers(player: Player) {
@@ -168,7 +177,7 @@ export class ManagePlayersComponent implements OnInit {
   }
 
   //to keep track of the value in search input
-  onKeyUp(searchVal:string){
+  onKeyUp(searchVal: string) {
     this.searchString = searchVal;
 
     this.activeDownSort = '';
