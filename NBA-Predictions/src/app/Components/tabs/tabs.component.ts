e549c865e4d4fb3b5e36820c502877c7ca96eba9
LTaylor137 <103200214@student.swin.edu.au>
2021-05-07 12:41:14 +1000

    tab styling fixed

--- NBA-Predictions/src/app/Components/tabs/tabs.component.ts
+++ NBA-Predictions/src/app/Components/tabs/tabs.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-
+import { TabsService } from '../../services/tabs.service';
 
 @Component({
   selector: 'app-tabs',
@@ -10,17 +10,20 @@ import { Router } from '@angular/router';
 export class TabsComponent implements OnInit {
 
   // TODO Fix 'selected-tab' logic for applying styles
-  selectedTab: string = 'TeamSummary';
+  selectedTab: string;
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, private tabsService: TabsService ) {
+    this.selectedTab = this.tabsService.currentTab;
+   }
 
   ngOnInit(): void {
   }
 
   Navigate(directory: string) {
-    this.selectedTab = directory;
-    // console.log(this.selectedTab);
+    this.tabsService.currentTab = directory;
+    console.log(this.selectedTab);
     this.router.navigateByUrl('/' + directory);
+   
   }
 
 }
